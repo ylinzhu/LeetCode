38833338d07183abea345af1483e453fab831d6e
yuanlinzhu <yuanlinzhu1995@gmail.com>
2019-03-29 23:22:27 +0800

    plusOne

--- src/easyAraays/PlusOne.java
+++ src/easyAraays/PlusOne.java
@@ -2,12 +2,21 @@ package easyAraays;
 
 /**
  * @Designation:
- * @Author: Yuanlinzhu
- * @Date: 2019/3/29 19:39
+ * @Author: Ylz
+ * @Date: 2019/3/29
  * @Version: 1.0
  */
 public class PlusOne {
-    public int[] plusOne(int[] digits) {
-        return digits;
+    public static int[] plusOne(int[] digits) {
+        int carry = 1;
+        for (int i = digits.length-1; i>= 0; i--) {
+            digits[i] += carry;
+            if (digits[i] <= 9)
+                return digits;
+            digits[i] = 0;
+        }
+        int[] ret = new int[digits.length+1];
+        ret[0] = 1;
+        return ret;
     }
 }
