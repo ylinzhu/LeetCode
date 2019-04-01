02eae33fd6b5e84ddf97a1e0583d8a28445d5506
yuanlinzhu <yuanlinzhu1995@gmail.com>
2019-04-01 22:37:25 +0800

    更改ignore配置

---
+++ src/easyAraays/IsValidSudoku.java
@@ -0,0 +1,27 @@
+package easyAraays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Designation:
+ * @Author: Ylz
+ * @Date: 2019/3/30
+ * @Version: 1.0
+ */
+public class IsValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet set = new HashSet();
+        for (int i=0;i<9;i++){
+            for (int j =0 ;j<9; j++){
+                char c = board[i][j];
+                if (c!= '.')
+                    if (!set.add(c+"in row"+i)||!set.add(c+"in colum"+j)||!set.add(c+"in blok"+i/3+"-"+j/3))
+                        return false;
+            }
+        }
+        return true;
+    }
+}
