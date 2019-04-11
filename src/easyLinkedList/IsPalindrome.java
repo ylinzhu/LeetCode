bcbf6aa64cc453dd7141c501b0ff30681437730a
yuanlinzhu <yuanlinzhu1995@gmail.com>
2019-04-11 23:57:04 +0800

    Default Changelist

---
+++ src/easyLinkedList/IsPalindrome.java
@@ -0,0 +1,22 @@
+package easyLinkedList;
+
+/**
+ * @Designation:
+ * @Author: Ylz
+ * @Date: 2019/4/11
+ * @Version: 1.0
+ */
+public class IsPalindrome {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null)
+            return true;
+        ListNode node =
+    }
+}
