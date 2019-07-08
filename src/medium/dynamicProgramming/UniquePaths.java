97d656e30b209a1f535c5988b94e4d0f0d748637
yuanlinzhu <yuanlinzhu1995@gmail.com>
2019-07-08 17:35:57 +0800

    uniquepahts

--- src/medium/dynamicProgramming/UniquePaths.java
+++ src/medium/dynamicProgramming/UniquePaths.java
@@ -9,7 +9,14 @@ package medium.dynamicProgramming;
 public class UniquePaths {
 
     public int uniquePaths(int m, int n) {
-
-        return 1;
+        if (m < 2|| n <2)return 1;
+        int[][] dp = new int[m][n];
+        for (int i = m -1; i >= 0; i--) {
+            for (int j = n -1; j >= 0; j--) {
+                if (i == m -1||j==n -1) dp[i][j] = 1;
+                else dp[i][j] = dp[i+1][j] + dp[j][i+1];
+            }
+        }
+        return dp[0][0];
     }
 }
