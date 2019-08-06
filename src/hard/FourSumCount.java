37fd4c9832a9b18ef5e717cf344ae18ffde26d55
yuanlinzhu <yuanlinzhu1995@gmail.com>
2019-08-06 11:16:43 +0800

    4数之和

--- src/hard/FourSumCount.java
+++ src/hard/FourSumCount.java
@@ -3,39 +3,39 @@ package hard;
 import java.util.HashMap;
 import java.util.Map;
 
-public class dd {
+public class FourSumCount {
 
-    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+    public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
         int res = 0;
-        int[] one =new int[A.length<<1];
-        int[] two =new int[A.length<<1];
+        int[] one =new int[A.length*A.length];
+        int[] two =new int[A.length*A.length];
         int index = 0;
+        Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < B.length; j++) {
                 one[index++] = A[i]+B[j];
             }
         }
 
+        index = 0;
         for (int i = 0; i < C.length; i++) {
             for (int j = 0; j < D.length; j++) {
                 two[index++] = C[i]+D[j];
             }
         }
-        Map<Integer,Integer> map = new HashMap<>();
 
         for (int i : one) {
             if (map.containsKey(i)){
-                map.put(i,map.get(i));
-            }
+                map.put(i,map.get(i)+1);
+            }else {
             map.put(i,1);
+            }
         }
         for (int i : two) {
-            if (map.containsKey(-i) & map.get(-i)>0){
-                map.put(-i,map.get(-i) -1);
-                res++;
+            if (map.containsKey(-i) ){
+                res += map.get(-i);
             }
         }
-
         return res;
     }
 }
